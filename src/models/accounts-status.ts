d4b80de49042ac10d1fef76f2c7f87a9b00abae3
deeborges <deyvisonsilvaborges@gmail.com>
2021-02-10 21:37:30 -0300

    feat: model - validacao com joi

---
+++ src/models/accounts-status.ts
@@ -0,0 +1,6 @@
+export enum AccountStatus {
+  CREATE = 100,
+  ACTIVE = 200,
+  SUSPENDED = 300,
+  REMOVED = 400,
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,19 @@
+import Joi from "joi";
+import { AccountStatus } from "./accounts-status";
 export interface IAccount {
   id: number;
   name: string;
   email: string;
   password: string;
-  status: number;
+  status: AccountStatus;
 }
+
+const AccountSchema = Joi.object({
+  id: Joi.number().integer().min(1),
+  name: Joi.string().alphanum().min(3).max(150).required(),
+  email: Joi.string().email().min(8).max(50).required(),
+  password: Joi.string().min(6).max(50),
+  status: Joi.number().integer().min(100).max(400),
+});
+
+export { AccountSchema };
